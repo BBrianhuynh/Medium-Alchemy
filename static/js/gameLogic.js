4925c972565159b929043ac6c42c197759eecbbb
owolexe <5325nathan@gmail.com>
2025-04-30 20:43:17 -0700

    Added collision detection

--- static/js/gameLogic.js
+++ static/js/gameLogic.js
@@ -1,31 +1,38 @@
-
+const radius = 50;
+const allItems = [];
 
 class Ingredient{
     constructor(itemNum, image, x, y){
         this.itemNum = itemNum;
 
-        this.x = x;
-        this.y = y;
-        this.id = "im1";
+        this.centerX = x;
+        this.centerY = y;
 
-        this.item = new Image();
+        this.id = "a" + allItems.length;
+
+        this.item = new Image(); //html image object
         
-        this.item.src = "C:/Users/5325n/OneDrive/Documents/CSE 108 Labs/FinalProject/static/fire-pixel-art-bonfire-pixelated-260nw-2309263493.webp";
+        //this.item.src = "{{ url_for('static', filename='img/fire-pixel-art-bonfire-pixelated-260nw-2309263493.webp') }}";
+        this.item.src = "/static/img/fire-pixel-art-bonfire-pixelated-260nw-2309263493.webp";
         this.item.id = this.id;
+        this.item.width = radius * 2;
+        this.item.height = radius * 2;
+
         this.item.style.position = "absolute";
         document.body.appendChild(this.item);
 
+        allItems[allItems.length] = this;
         this.dragItem();
         
         
     }
 
-    get xPos() {
-        return this.x;
+    get x() {
+        return this.centerX;
     }
 
-    get yPos() {
-        return this.y;
+    get y() {
+        return this.centerY;
     }
 
 
@@ -53,17 +60,38 @@ class Ingredient{
         function dragElement(e){
             pos1 = mouseX - e.clientX;
             pos2 = mouseY - e.clientY;
+            
+
             mouseX = e.clientX;
             mouseY = e.clientY;
 
             that.item.style.top = (that.item.offsetTop - pos2) + "px";
             that.item.style.left = (that.item.offsetLeft - pos1) + "px";
 
+            that.centerY = (that.item.offsetTop - pos2) + radius;
+            that.centerX = (that.item.offsetLeft - pos1) + radius;
+
+            document.getElementById("x").innerHTML = "x: " +  that.centerY;
+            document.getElementById("y").innerHTML = "y: " +  that.centerX;
+
+            checkCollision(that.centerX, that.centerY);
+
         }
 
         function stopDrag() {
             document.onmouseup = null;
             document.onmousemove = null;
+        }
+
+        function checkCollision(x, y) {
+            for (let i = 0; i < allItems.length; i ++) {
+                let distance = Math.sqrt((x - allItems[i].x) ** 2 + (y - allItems[i].y) ** 2);
+                if (distance > 0 && distance <= radius) {
+                    console.log("collision!");
+                }
+                
+            }
+            return (null);
         }
 
 
@@ -73,4 +101,5 @@ class Ingredient{
 
 document.addEventListener("DOMContentLoaded", function () {
     const fire = new Ingredient(0, "a", 0, 0);
+    const water = new Ingredient(0, "b" , 0, 0);
 });
