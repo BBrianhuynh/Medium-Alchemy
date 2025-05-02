5032b2ab4fa201ea6c0299f79a89d9be85d8e0a4
bbrianhuynh <brianhuynh1028@gmail.com>
2025-05-01 22:24:10 -0700

    Added inventory panel and combine elements function when collided, needs testing

--- static/js/gameLogic.js
+++ static/js/gameLogic.js
@@ -1,8 +1,11 @@
 const radius = 50;
+const elementsOnScreen = [];
+const discovered = [];
+const recipes = [];
 const allItems = [];
 
-class Ingredient{
-    constructor(itemNum, image, x, y){
+class Ingredient {
+    constructor(itemNum, image, x, y) {
         this.itemNum = itemNum;
 
         this.centerX = x;
@@ -11,7 +14,7 @@ class Ingredient{
         this.id = "a" + allItems.length;
 
         this.item = new Image(); //html image object
-        
+
         //this.item.src = "{{ url_for('static', filename='img/fire-pixel-art-bonfire-pixelated-260nw-2309263493.webp') }}";
         this.item.src = "/static/img/fire-pixel-art-bonfire-pixelated-260nw-2309263493.webp";
         this.item.id = this.id;
@@ -23,8 +26,8 @@ class Ingredient{
 
         allItems[allItems.length] = this;
         this.dragItem();
-        
-        
+
+
     }
 
     get x() {
@@ -35,11 +38,9 @@ class Ingredient{
         return this.centerY;
     }
 
-
-
     dragItem() {
         var mouseX, mouseY, pos1, pos2;
-        if (document.getElementById(this.id)){
+        if (document.getElementById(this.id)) {
             document.getElementById(this.id).onmousedown = dragDown;
         }
         else {
@@ -57,10 +58,9 @@ class Ingredient{
             document.onmousemove = dragElement;
         }
 
-        function dragElement(e){
+        function dragElement(e) {
             pos1 = mouseX - e.clientX;
             pos2 = mouseY - e.clientY;
-            
 
             mouseX = e.clientX;
             mouseY = e.clientY;
@@ -71,8 +71,8 @@ class Ingredient{
             that.centerY = (that.item.offsetTop - pos2) + radius;
             that.centerX = (that.item.offsetLeft - pos1) + radius;
 
-            document.getElementById("x").innerHTML = "x: " +  that.centerY;
-            document.getElementById("y").innerHTML = "y: " +  that.centerX;
+            document.getElementById("x").innerHTML = "x: " + that.centerY;
+            document.getElementById("y").innerHTML = "y: " + that.centerX;
 
             checkCollision(that.centerX, that.centerY);
 
@@ -81,25 +81,60 @@ class Ingredient{
         function stopDrag() {
             document.onmouseup = null;
             document.onmousemove = null;
+            const collidedElementId = checkCollision(that.centerX, that.centerY)
+            if (collidedElementId) {
+                const newItemId = combine(that.id, collidedElementId)
+                if (newItemId != null) {
+                    this.itemId = newItemId;
+                     
+                }
+            }
         }
 
+
         function checkCollision(x, y) {
-            for (let i = 0; i < allItems.length; i ++) {
+            for (let i = 0; i < allItems.length; i++) {
                 let distance = Math.sqrt((x - allItems[i].x) ** 2 + (y - allItems[i].y) ** 2);
                 if (distance > 0 && distance <= radius) {
                     console.log("collision!");
+                    return allItems[i].id;
                 }
-                
+
             }
-            return (null);
+            return null;
         }
 
 
     }
 
+    destroy() {
+        document.body.removeChild(this.item);
+        const index = allItems.indexOf(this);
+        if (index != -1)
+            allItems.splice(index, 1);
+    }
+
+}
+
+function combine(elementOneId, elementTwoId) {
+    for (const itemId in allItems) {
+        const item = allItems[itemId];
+        const combinations = item.parents;
+        for (const combination in combinations) {
+            if ((combination[0] == elementOneId && combination[1] == elementTwoId) || (combination[0] == elementTwoId && combination[1] == elementOneId)) {
+                return itemId;
+            }
+        }
+    }
+    return null;
+}
+
+function clear() {
+    elementsOnScreen = [];
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     const fire = new Ingredient(0, "a", 0, 0);
-    const water = new Ingredient(0, "b" , 0, 0);
+    const water = new Ingredient(0, "b", 0, 0);
+    const air = new Ingredient(0, "c", 0, 0)
 });
